f9124d1ac2d47bf05a0f8297cdef20bc7c287d8f
Imran Hossain <imransardar122@gmail.com>
2024-11-24 20:25:19 +0600

    fix

--- src/components/common/wrapper/SectionWrapper.tsx
+++ src/components/common/wrapper/SectionWrapper.tsx
@@ -1,6 +1,6 @@
-import { ReactNode, FC } from "react";
 import { cn } from "@/lib/utils";
 import { useFontShifter } from "@/utils/hooks/useFontShifter";
+import { FC, ReactNode } from "react";
 interface ISectionWrapperProps {
   children: ReactNode;
   className?: string;
@@ -9,7 +9,7 @@ const SectionWrapper: FC<ISectionWrapperProps> = ({ children, className }) => {
   return (
     <section
       className={cn(
-        "max-w-[1300px] flex flex-col items-center my-[150px]",
+        "w-[1300px] flex flex-col items-center my-[150px]",
         className,
         useFontShifter()
       )}
--- src/pages/dashboard/counterRole/counter-wise-report/CounterWiseReport.tsx
+++ src/pages/dashboard/counterRole/counter-wise-report/CounterWiseReport.tsx
@@ -14,15 +14,29 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useGetSupervisorCoachDetailsQuery } from "@/store/api/superviosr/supervisorExpenseApi";
+import { useToast } from "@/components/ui/use-toast";
+import {
+  useCreateCounterReportSubmitMutation,
+  useGetSupervisorCoachDetailsQuery,
+} from "@/store/api/superviosr/supervisorExpenseApi";
 import { useGetModalCoachInfoByDateQuery } from "@/store/api/vehiclesSchedule/coachConfigurationApi";
+import { useCustomTranslator } from "@/utils/hooks/useCustomTranslator";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { useReactToPrint } from "react-to-print";
+import logo from "../../../../assets/longeng.png";
 
+import PdfCounterReport from "../../pdf/PdfCounterReport";
+import PrintCounterReport from "../../printLabel/PrintCounterReport";
 export default function CounterWiseReport() {
+  //const tableRef = useRef<HTMLDivElement>(null);
+
   const today = new Date();
+  const { toast } = useToast();
+  const { translate } = useCustomTranslator();
   const user = useSelector((state: any) => state.user); // Access logged-in user data
   const [calendarOpen, setCalendarOpen] = useState(false);
   //const { toast } = useToast();
@@ -42,6 +56,10 @@ export default function CounterWiseReport() {
   // Fetch report data for the selected coach
   const { data: reportsData, isLoading: reportLoading } =
     useGetSupervisorCoachDetailsQuery(selectedCoachId || 0);
+
+  //report submit
+  const [createCounterReportSubmit] = useCreateCounterReportSubmitMutation({});
+
   useEffect(() => {
     if (todaysCoachInfo?.data) {
       setLocalData(todaysCoachInfo.data);
@@ -56,141 +74,225 @@ export default function CounterWiseReport() {
       setFilteredCounterData(counterData || null);
     }
   }, [reportsData, user]);
-
+  const onSubmit = async () => {
+    const result = await createCounterReportSubmit({
+      coachConfigId: selectedCoachId,
+    });
+    if (result?.data?.success) {
+      toast({
+        title: translate("রিপোর্ট জমা করা হয়েছে", "Report submitted"),
+        description: translate(
+          "রিপোর্ট জমা সফলভাবে যোগ করা হয়েছে।",
+          "Report Submitted successfully."
+        ),
+      });
+    }
+  };
+  const printSaleRef = useRef(null);
+  const handlePrint = useReactToPrint({
+    content: () => printSaleRef.current,
+    documentTitle: `Counter Wise Report`,
+  });
   if (isTodaysCoachLoading || reportLoading) {
     return <DetailsSkeleton />;
   }
   return (
-    <FormWrapper
-      heading="Counter-Wise Report"
-      subHeading="View counter-wise details for the selected coach."
-    >
-      <div className="flex gap-5">
-        {/* Date Selector */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium mb-2">Select Date</label>
-          <Popover open={calendarOpen} onOpenChange={setCalendarOpen}>
-            <PopoverTrigger asChild>
-              <Button variant="outline" className="w-full text-left">
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {selectedDate
-                  ? format(selectedDate, "yyyy-MM-dd")
-                  : "Select a date"}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent align="start">
-              <Calendar
-                mode="single"
-                selected={selectedDate || new Date()}
-                onSelect={(date) => {
-                  setSelectedDate(date ?? null); // Handle undefined by setting to null
-                  setCalendarOpen(false);
-                }}
-              />
-            </PopoverContent>
-          </Popover>
+    <div>
+      <FormWrapper
+        heading="Counter-Wise Report"
+        subHeading="View counter-wise details for the selected coach."
+      >
+        <div className="flex justify-between items-center">
+          <div className="flex gap-5 ">
+            {/* Date Selector */}
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">
+                Select Date
+              </label>
+              <Popover open={calendarOpen} onOpenChange={setCalendarOpen}>
+                <PopoverTrigger asChild>
+                  <Button variant="outline" className="w-full text-left">
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {selectedDate
+                      ? format(selectedDate, "yyyy-MM-dd")
+                      : "Select a date"}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent align="start">
+                  <Calendar
+                    mode="single"
+                    selected={selectedDate || new Date()}
+                    onSelect={(date) => {
+                      setSelectedDate(date ?? null); // Handle undefined by setting to null
+                      setCalendarOpen(false);
+                    }}
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+
+            {/* Coach Selector */}
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">
+                Select Coach
+              </label>
+              <Select
+                value={selectedCoachId?.toString() || undefined}
+                onValueChange={(value) => setSelectedCoachId(parseInt(value))}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a Coach" />
+                </SelectTrigger>
+                <SelectContent>
+                  {localData.map((coach: any) => (
+                    <SelectItem key={coach.id} value={coach.id.toString()}>
+                      {coach.coachNo}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          <div className="flex gap-3 mt-6">
+            <PDFDownloadLink
+              document={
+                <PdfCounterReport
+                  filteredCounterData={filteredCounterData}
+                  reportsData={reportsData}
+                  title="Sample Table Report"
+                  logo={logo}
+                />
+              }
+              fileName="counter-wise-report.pdf"
+            >
+              {
+                //@ts-ignore
+                (params) => {
+                  const { loading } = params;
+                  return loading ? (
+                    <Button
+                      disabled
+                      className="transition-all duration-150"
+                      variant="default"
+                      size="lg"
+                    >
+                      Pdf
+                    </Button>
+                  ) : (
+                    <Button variant="default" size="lg">
+                      Pdf
+                    </Button>
+                  );
+                }
+              }
+            </PDFDownloadLink>
+
+            <Button size="lg" onClick={handlePrint}>
+              PRINT
+            </Button>
+          </div>
         </div>
 
-        {/* Coach Selector */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium mb-2">Select Coach</label>
-          <Select
-            value={selectedCoachId?.toString() || undefined}
-            onValueChange={(value) => setSelectedCoachId(parseInt(value))}
-          >
-            <SelectTrigger className="w-full">
-              <SelectValue placeholder="Select a Coach" />
-            </SelectTrigger>
-            <SelectContent>
-              {localData.map((coach: any) => (
-                <SelectItem key={coach.id} value={coach.id.toString()}>
-                  {coach.coachNo}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-
-      {/* Table Display */}
-      {filteredCounterData && (
-        <div className="mt-4">
-          <table className="min-w-full border-collapse border border-gray-200">
-            <thead>
-              <tr>
-                <th className="border px-4 py-2">Coach No</th>
-                <th className="border px-4 py-2">Total Seat</th>
-                <th className="border px-4 py-2">Total Taka</th>
-                <th className="border px-4 py-2">Commission (Tk)</th>
-                <th className="border px-4 py-2">Payable Amount (Tk)</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="text-center">
-                <td className="border px-4 py-2">
-                  {reportsData?.data?.coachInfo?.coachNo || "N/A"}
-                </td>
-                <td className="border px-4 py-2">
-                  {filteredCounterData.totalSeat || 0}
-                </td>
-                <td className="border px-4 py-2">
-                  {filteredCounterData.totalAmount || 0}
-                </td>
-                <td className="border px-4 py-2">
-                  {filteredCounterData.commission || 0}
-                </td>
-                <td className="border px-4 py-2">
-                  {filteredCounterData.totalAmount -
-                    filteredCounterData.commission || 0}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-
-          {/* Additional Table for Passenger Details */}
-          <div className="mt-6">
-            <h3 className="text-lg font-semibold mb-4">Passenger Details</h3>
+        {/* Table Display */}
+        {filteredCounterData && (
+          <div ref={printSaleRef} className="mt-4">
             <table className="min-w-full border-collapse border border-gray-200">
               <thead>
                 <tr>
-                  <th className="border px-4 py-2">Passenger Name</th>
-                  <th className="border px-4 py-2">Phone</th>
-                  <th className="border px-4 py-2">Seats Booked</th>
-                  <th className="border px-4 py-2">Sold By</th>
+                  <th className="border px-4 py-2">Counter Name</th>
+                  <th className="border px-4 py-2">Coach No</th>
+                  <th className="border px-4 py-2">Total Seat</th>
+                  <th className="border px-4 py-2">Total Taka</th>
+                  <th className="border px-4 py-2">Commission (Tk)</th>
+                  <th className="border px-4 py-2">Payable Amount (Tk)</th>
                 </tr>
               </thead>
               <tbody>
-                {filteredCounterData.orderDetails.map(
-                  (order: any, index: number) => (
-                    <tr key={index} className="text-center">
-                      <td className="border px-4 py-2">
-                        {order.customerName || "N/A"}
-                      </td>
-                      <td className="border px-4 py-2">
-                        {order.phone || "N/A"}
-                      </td>
-                      <td className="border px-4 py-2">
-                        {order.orderSeat
-                          .map((seat: any) => seat.seat)
-                          .join(", ")}
-                      </td>
-                      <td className="border px-4 py-2">
-                        {order.user?.userName}
-                      </td>
-                    </tr>
-                  )
-                )}
+                <tr className="text-center">
+                  <td className="border px-4 py-2">
+                    {filteredCounterData.counterName || "N/A"}
+                  </td>
+                  <td className="border px-4 py-2">
+                    {reportsData?.data?.coachInfo?.coachNo || "N/A"}
+                  </td>
+                  <td className="border px-4 py-2">
+                    {filteredCounterData.totalSeat || 0}
+                  </td>
+                  <td className="border px-4 py-2">
+                    {filteredCounterData.totalAmount || 0}
+                  </td>
+                  <td className="border px-4 py-2">
+                    {filteredCounterData.commission || 0}
+                  </td>
+                  <td className="border px-4 py-2">
+                    {filteredCounterData.totalAmount -
+                      filteredCounterData.commission || 0}
+                  </td>
+                </tr>
               </tbody>
             </table>
-          </div>
-        </div>
-      )}
 
-      {!filteredCounterData && (
-        <div className="mt-4 text-red-500 text-center">
-          No data available for your counter.
-        </div>
-      )}
-    </FormWrapper>
+            {/* Additional Table for Passenger Details */}
+            <div className="mt-6">
+              <h3 className="text-lg font-semibold mb-4">Passenger Details</h3>
+              <table className="min-w-full border-collapse border border-gray-200">
+                <thead>
+                  <tr>
+                    <th className="border px-4 py-2">Passenger Name</th>
+                    <th className="border px-4 py-2">Phone</th>
+                    <th className="border px-4 py-2">Seats Booked</th>
+                    <th className="border px-4 py-2">Sold By</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredCounterData.orderDetails.map(
+                    (order: any, index: number) => (
+                      <tr key={index} className="text-center">
+                        <td className="border px-4 py-2">
+                          {order.customerName || "N/A"}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {order.phone || "N/A"}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {order.orderSeat
+                            .map((seat: any) => seat.seat)
+                            .join(", ")}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {order.user?.userName}
+                        </td>
+                      </tr>
+                    )
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        )}
+        {filteredCounterData &&
+          filteredCounterData?.counterType !== "Own_Counter" &&
+          !filteredCounterData.reportSubmitStatus && (
+            <Button type="submit" onClick={onSubmit} className="mt-3" size="lg">
+              Submit
+            </Button>
+          )}
+
+        {!filteredCounterData && (
+          <div className="mt-4 text-red-500 text-center">
+            No data available for your counter.
+          </div>
+        )}
+      </FormWrapper>
+      <div className="invisible hidden -left-full">
+        <PrintCounterReport
+          filteredCounterData={filteredCounterData}
+          ref={printSaleRef}
+          reportsData={reportsData}
+          title="Sample Table Report"
+          logo={logo}
+        />
+      </div>
+    </div>
   );
 }
