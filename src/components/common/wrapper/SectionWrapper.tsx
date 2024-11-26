09dd2dd72f18aa9e3331ae720c165864b9958013
Imran Hossain <imransardar122@gmail.com>
2024-11-26 15:37:43 +0600

    fix

--- src/components/common/wrapper/SectionWrapper.tsx
+++ src/components/common/wrapper/SectionWrapper.tsx
@@ -9,7 +9,7 @@ const SectionWrapper: FC<ISectionWrapperProps> = ({ children, className }) => {
   return (
     <section
       className={cn(
-        "max-w-[1400px] mx-auto flex flex-col items-center my-[150px]",
+        "w-[1300px] flex flex-col items-center my-[150px]",
         className,
         useFontShifter()
       )}
