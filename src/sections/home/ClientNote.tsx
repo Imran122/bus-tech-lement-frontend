630fd24f934e6aca85205602191b5eb99d87c987
Imran Hossain <imransardar122@gmail.com>
2024-12-02 11:59:38 +0600

    fix

--- src/sections/home/ClientNote.tsx
+++ src/sections/home/ClientNote.tsx
@@ -1,21 +1,28 @@
 import { Heading } from "@/components/common/typography/Heading";
-import { Paragraph } from "@/components/common/typography/Paragraph";
 import { useGetSingleCMSQuery } from "@/store/api/cms/contentManagementApi";
 import { useCustomTranslator } from "@/utils/hooks/useCustomTranslator";
 
 const ClientNote = () => {
   const { translate } = useCustomTranslator();
   const { data: singleCms } = useGetSingleCMSQuery({});
+
+  // Get the translated description
+  const description = translate(
+    singleCms?.data?.homePageDescriptionBangla,
+    singleCms?.data?.homePageDescription
+  );
+
   return (
     <div className="h-auto mx-auto border-2 rounded-lg border-secondary p-5 lg:p-16 w-full flex flex-col items-center justify-between">
-      <Heading size={"h4"} className="text-center">"Iconic Express"</Heading>
+      <Heading size={"h4"} className="text-center">
+        "Iconic Express"
+      </Heading>
 
-      <Paragraph size={"md"} className="text-center w-full lg:w-5/6">
-        {translate(
-          singleCms?.data?.homePageDescriptionBangla,
-          singleCms?.data?.homePageDescription
-        )}
-      </Paragraph>
+      {/* Render HTML content safely */}
+      <div
+        className="text-center w-full lg:w-5/6"
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
     </div>
   );
 };
