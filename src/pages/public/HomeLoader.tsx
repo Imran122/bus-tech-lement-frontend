c76849bca715dbc5ded8edea8195eda4743ef8e6
Imran Hossain <imransardar122@gmail.com>
2024-12-02 15:46:05 +0600

    fix1

--- src/pages/public/HomeLoader.tsx
+++ src/pages/public/HomeLoader.tsx
@@ -4,7 +4,7 @@ import "./HomeLoader.css";
 const HomeLoader = () => {
   return (
     <div className="flex justify-center items-center min-h-screen pb-20 bg-gray-100">
-      <div className="main">
+      <div className="main animate-pulse">
         {/* Rotating Bus */}
         <div className="circle">
           <img src={busImage} alt="bus" className="bus-image" />
